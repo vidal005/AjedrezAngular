f6fc4c80882d8878e9cd11271ae2f3fc65c74805
vidal005 <vidal051091@gmail.com>
2020-03-03 13:26:27 +0100

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +15,27 @@ export class AppComponent {
   private stompClient;
 
   constructor(){
-    //this.initializeWebSocketConnection();
+    this.initializeWebSocketConnection();
+  }
+
+  initializeWebSocketConnection(){
+    let ws = new SockJS(this.serverUrl);
+    this.stompClient = Stomp.over(ws);
+    let that = this;
+    this.stompClient.connect({}, function(frame) {
+      that.stompClient.subscribe("/chat", (message) => {
+        if(message.body) {
+          $(".chat").append("<div class='message'>"+message.body+"</div>")
+          console.log(message.body);
+        }
+      });
+    });
+  }
+
+  sendMessage(message){
+    this.stompClient.send("/app/send/message" , {}, message);
+    $('#input').val('');
   }
 
   
   }
-
-
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -49,13 +49,14 @@
  *  with the following flag, it will bypass `zone.js` patch for IE/Edge
  *
  *  (window as any).__Zone_enable_cross_context_check = true;
- *
+ *  (window as any).global = window;
  */
 
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
+(window as any).global = window;
 
 
 /***************************************************************************************************
